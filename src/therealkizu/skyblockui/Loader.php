1be0981ef2a773358fa924961abc795a59489ba9
TheRealKizu <therealkizu@protonmail.com>
2021-02-10 12:02:33 +0800

    chore: update copyright

--- src/therealkizu/skyblockui/Loader.php
+++ src/therealkizu/skyblockui/Loader.php
@@ -2,7 +2,7 @@
 
 /**
 *                  SkyBlockUI
- * Copyright (C) 2019-2020 TheRealKizu
+ * Copyright (C) 2019-2021 TheRealKizu
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
